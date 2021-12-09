898c1486fbb28b27cab0fa7b3fcf4c8a03e5f38b
unknown <johnhope.dawa@yahoo.com>
2021-12-08 19:52:22 -0800

    Hell yeah

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+function upHover(){
+    document.getElementsByClassName("first")[0].style.fontSize = "2em";
+    document.getElementsByClassName("first")[0].style.opacity = "100%";
+    document.getElementsByClassName("second")[0].style.fontSize = ".97em";
+    document.getElementsByClassName("second")[0].style.opacity = "50%";
+    
+
+}
+
+function upHoverOut() {
+    document.getElementsByClassName("first")[0].style.fontSize = ".97em";
+    document.getElementsByClassName("first")[0].style.opacity = "50%";
+    document.getElementsByClassName("second")[0].style.fontSize = "2em";
+    document.getElementsByClassName("second")[0].style.opacity = "100%";
+}
