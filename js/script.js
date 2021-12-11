5c62e8a4922a7caa5766c1fdcd59f40ee2982079
unknown <johnhope.dawa@yahoo.com>
2021-12-11 13:41:55 -0800

    New design

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 
 function upHover(){
-    document.getElementsByClassName("first")[0].style.fontSize = "2em";
+    document.getElementsByClassName("first")[0].style.fontSize = "em";
     document.getElementsByClassName("first")[0].style.opacity = "100%";
     document.getElementsByClassName("second")[0].style.fontSize = ".97em";
+    document.getElementsByClassName("second")[0].style.top = "38.5%"; 
     document.getElementsByClassName("second")[0].style.opacity = "50%";
     
 
@@ -13,6 +14,7 @@ function upHover(){
 function upHoverOut() {
     document.getElementsByClassName("first")[0].style.fontSize = ".97em";
     document.getElementsByClassName("first")[0].style.opacity = "50%";
+    document.getElementsByClassName("second")[0].style.top = "25.5%";
     document.getElementsByClassName("second")[0].style.fontSize = "2em";
     document.getElementsByClassName("second")[0].style.opacity = "100%";
 }
